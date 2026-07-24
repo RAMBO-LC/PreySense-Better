a7304996625737449d3645634b109215740ceb31
RAMBO-LC <rambo4code@gmail.com>
2026-07-24 13:20:20 +0530

    Delete update_asar.ps1

--- update_asar.ps1
+++
@@ -1,5 +0,0 @@
-Get-Process | Where-Object { $_.ProcessName -match 'PreySense|electron|node' } | Stop-Process -Force -ErrorAction SilentlyContinue
-Start-Sleep 3
-Copy-Item -Path 'PreySense.Desktop/release/win-unpacked/resources/app.asar' -Destination 'build_output/win-unpacked/resources/app.asar' -Force
-Write-Host "Copied: $((Get-Item 'build_output/win-unpacked/resources/app.asar').Length) bytes"
-Remove-Item 'make_ico.ps1' -ErrorAction SilentlyContinue
